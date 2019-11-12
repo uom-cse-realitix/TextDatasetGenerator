d43d184efac3fa52c9211731636e6e137078b6d3
ashen <desilva.ashen3@gmail.com>
2019-11-12 18:57:33 +0530

    Text generation from librispeech transcripts for auto encoder added

---
+++ librispeech_negative_data_generator.py
@@ -0,0 +1,33 @@
+import os
+import random
+
+def get_transcript_file_list(DIR_PATH):
+    transcript_file_paths = []
+    for root, dirs, files in os.walk(DIR_PATH):
+        for file in files:
+            if file.endswith(".txt"):
+                transcript_file_paths.append(os.path.join(root, file))
+    return transcript_file_paths
+
+def get_transcripts(FILE_PATH):
+    with open(FILE_PATH, 'r') as text_file:
+        for line in text_file:
+            r = random.randint(1, int(3 * (len(line.split(" ")) / 4)))
+            t_lines.append(line.split(' ', r)[r].lower())
+
+def write_dataset_file(DATASET_FILE):
+    with open(DATASET_FILE, 'a+') as file:
+        for line in t_lines:
+            file.write(line)
+
+
+DATASET_FILE = '/home/ashen/Documents/Libri_dataset.txt'
+t_lines = []
+
+transcripts = get_transcript_file_list("/media/ashen/52DAB5C7DAB5A81D/Users/Ashen/Downloads/LibriSpeech/train-clean-100")
+
+for file_no in range(len(transcripts)):
+    get_transcripts(transcripts[file_no])
+    if len(t_lines) > 10000 or file_no == len(transcripts) - 1:
+        write_dataset_file(DATASET_FILE)
+        t_lines = []
